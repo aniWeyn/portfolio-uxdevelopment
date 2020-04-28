f1e24e2e5377509d2fcb34998ad0bd8c45c5bdc2
Sabina Niewiadomska <ext_sabina.niewiadomska@aprila.no>
2020-04-28 16:02:52 +0200

    kpmg update

--- src/views/KPMG.vue
+++ src/views/KPMG.vue
@@ -1,11 +1,60 @@
 <template>
-  <div class="about">
+  <div class>
     <h2>KPMG</h2>
-        <p class="project-description"> In KPMG I had a chance to work with the smartest auditors from KPMG Norway, Finland and Sweden.
-      </p>
+    <p
+      class="project-description"
+    >In KPMG I had a chance to work with the smartest auditors from KPMG Norway, Finland and Sweden. </p>
+                <div class="image-description">
+      <div>
+        <img
+          src="~@/assets/img/kpmg-prototype.png"
+          alt="Gif of a switcher"
+          width="500"
+          height="auto"
+        />
+        <p class="paragraph">Example of prototype</p>
+      </div>
+      <div></div>
+    </div>
+
+        <div class="image-description">
+      <div>
+        <img
+          src="~@/assets/img/kpmg-switcher2.png"
+          alt="Photo of the scale from the front"
+          width="100"
+          height="auto"
+        />
+        <img
+          src="~@/assets/img/kpmg-switcher3.png"
+          alt="Photo of the scale from the front"
+          width="100"
+          height="auto"
+        />
+        <img
+          src="~@/assets/img/kpmg-switcher1.png"
+          alt="Photo of the scale from the front"
+          width="120"
+          height="auto"
+        />
+        <p class="paragraph">Example of prototype.</p>
+      </div>
+
+    </div>
+        <div class="image-description">
+      <div>
+        <img
+          src="~@/assets/img/kpmg-switcher.gif"
+          alt="Gif of a switcher"
+          width="500"
+          height="auto"
+        />
+        <p class="paragraph">Toggle button</p>
+      </div>
+      <div></div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
 </style>
