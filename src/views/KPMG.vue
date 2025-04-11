0b8199f5c5988f1eece8a7cfc235f5a476fafe21
aniWeyn <4niweyn@gmail.com>
2025-04-11 13:29:27 +0200

    updated KPMG side

--- src/views/KPMG.vue
+++ src/views/KPMG.vue
@@ -2,9 +2,11 @@
   <div class>
     <h2>KPMG</h2>
     <p class="project-description">
-      In KPMG I had a chance to help a highly international team of the smartest
-      auditors. My colleagoues represented KPMG Norway, KPMG Finland and KPMG
-      Sweden.
+      At KPMG, I had the opportunity to support a highly international team of
+      exceptionally skilled auditors, collaborating with colleagues from KPMG
+      Norway, Finland, and Sweden. KPMG Norway was prototyping a new solution
+      they planned to pitch to KPMG Global. With our support, the proposal was
+      successfully accepted and later adopted by KPMG Global.
     </p>
     <b>Roles:</b> UX / UI Designer, VueJS Developer
     <!--<div class="image-description">
@@ -19,6 +21,17 @@
       </div>
       <div></div>
     </div>-->
+    <h4>UX / UI Designer</h4>
+    For KPMG Norway, I had the opportunity to design a high-fidelity, clickable
+    prototype in Figma that represented the vision for the future solution. I
+    was responsible for balancing the creation of a unique visual identity for
+    the project while ensuring alignment with KPMG's overall brand guidelines.
+    <br />
+    <br />
+    What made this project particularly interesting was that I was designing for
+    myself as the developer. This meant I had to be especially mindful of the
+    technical feasibility of my designs, ensuring that what I created in Figma
+    could be realistically and efficiently implemented in code.
 
     <div class="image-description">
       <div>
@@ -40,9 +53,25 @@
           width="120"
           height="auto"
         />
-        <p class="paragraph">Example of prototype.</p>
+        <p class="paragraph">
+          Small preview of one of the UI elements I designed.
+        </p>
       </div>
     </div>
+    <h4>VueJS Developer</h4>
+    After the design phase, I contributed to the project as part of a two-person
+    development team—myself as the frontend developer, alongside a senior
+    backend developer. What made the KPMG project unique was that we were not
+    granted access to the backend codebase. This constraint required a high
+    degree of flexibility and close collaboration between the frontend and
+    backend teams to ensure seamless integration and functionality. <br /><br />
+
+    Because I was responsible for both designing and implementing the frontend,
+    this experience strengthened my realistic mindset in design. It made me more
+    mindful of creating solutions that were not only user-friendly but also
+    technically feasible—prioritizing simplicity, clarity, and reliability in
+    both the design and the code.
+
     <!--<div class="image-description">
       <div>
         <img
@@ -58,5 +87,4 @@
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
