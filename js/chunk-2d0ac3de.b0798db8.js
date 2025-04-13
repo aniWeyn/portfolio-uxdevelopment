087586cb66d2d9f9066937d79141865cd4a5b067
aniWeyn <aniWeyn@users.noreply.github.com>
2025-04-13 13:42:22 +0000

    deploy: f92154a49f38e828055b78fbb0c7e6f8cce26de1

--- js/chunk-2d0ac3de.b0798db8.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ac3de"],{1966:function(e,t,n){"use strict";n.r(t);var c=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h2",[e._v("Conductor")]),n("h3",[e._v("UX Design")]),n("h4",[e._v("Methods")]),n("h3",[e._v("Development")]),n("h4",[e._v("VueJS")])])}],a=n("2877"),u={},o=Object(a["a"])(u,c,s,!1,null,"0659d31a",null);t["default"]=o.exports}}]);
-//# sourceMappingURL=chunk-2d0ac3de.b0798db8.js.map
--- js/chunk-2d21f337.001732c2.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21f337"],{d98d:function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h2",[e._v("Orkla")]),n("h3",[e._v("UX Design")]),n("h4",[e._v("Methods")]),n("h3",[e._v("Development")]),n("h4",[e._v("VueJS")])])}],c=n("2877"),r={},u=Object(c["a"])(r,s,a,!1,null,"5464e59a",null);t["default"]=u.exports}}]);
-//# sourceMappingURL=chunk-2d21f337.001732c2.js.map
