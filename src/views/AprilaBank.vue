99636debf1ee716e5d345b8e2349bfea18666c56
aniWeyn <4niweyn@gmail.com>
2025-04-13 18:16:57 +0200

    updated text about aprila bank

--- src/views/AprilaBank.vue
+++ src/views/AprilaBank.vue
@@ -2,25 +2,102 @@
   <div class="about">
     <h2>AprilaBank</h2>
     <p class="project-description">
-      AprilaBank is dynamic and modern norwegian startup based in Oslo. I had a
-      chance to work there as UX Designer as well as front-end developer.
+      Aprila Bank is a dynamic and modern Norwegian startup based in Oslo. I had
+      the opportunity to work there as both a UX Designer and a Front-End
+      Developer.
     </p>
-    <b>Roles:</b> UX / UI Designer, Design System Developer, VueJS Developer
-    <h4>Design System</h4>
-    <b>Technologies: </b> Storybook, WebComponents
-    <p>I was developing Design System elements created by Lead UX Designer.</p>
-    <h4>UX / UI Design</h4>
-    <b>Technologies: </b> Sketch, Miro
+    <b>Roles:</b> UX / UI Designer, Design System Developer, VueJS Developer,
+    Blazor Developer
+
+    <h4>UX / UI Designer</h4>
+    <b>Technologies: </b> Sketch, Miro, OOUX, journey mapping, observations,
+    co-designing
     <p>
-      I was running internal research to improve their internal application
-      (Product Journey Maps, interviews, observations). Based on my research I
-      was preparing the designs for development team.
+      I conducted internal research to improve Aprila Bank’s internal
+      application, using methods such as product journey mapping, interviews,
+      and user observations. Based on these insights, I prepared design
+      solutions for the development team.
     </p>
+    <p>
+      I worked alongside the UX Lead to improve the integration of UX
+      methodologies into the company’s processes—such as continuous
+      development—and to strengthen UX understanding among developers and
+      management.
+    </p>
+    <p>
+      I was involved in designing new solutions using Object-Oriented UX and
+      co-design methodologies. These approaches helped create more structured,
+      user-centered designs and fostered a shared understanding across the team.
+    </p>
+
+    <h4>Design System</h4>
+    <b>Technologies: </b> Storybook, WebComponents, CSS Components, MDX, BEM,
+    <p>
+      Aprila Design System provided front-end developers & engineers a
+      collection of reusable HTML and CSS partials to build websites and user
+      interfaces. Adopting the library enabled developers to use consistent
+      markup, styles, and behavior in prototype and production work. I had a
+      chance to join this project and continue its' development.
+    </p>
+    <p>
+      The Aprila design system was documented using Storybook, with component
+      stories written in the MDX format. This allowed for rich, interactive
+      documentation that clearly communicated the usage and behavior of each
+      component.
+    </p>
+    <p>
+      The design system components were developed in two versions—Web Components
+      and pure CSS components—to ensure easy integration with Blazor.
+    </p>
+    <p>
+      I was responsible for developing new components based on the UX Lead’s
+      guidelines, documenting the design system, and guiding developers in its
+      usage. I also maintained the changelog, managed versioning, and handled
+      distribution of the system as an NPM package.
+    </p>
+    <div class="image-description">
+      <div>
+        <img
+          src="~@/assets/img/aprila_designsystem.png"
+          alt="Screenshot of Storybook for Aprila Bank"
+          width="100%"
+          height="auto"
+        />
+        <p class="paragraph">
+          The screenshot above highlights a part of the design system I worked
+          on for Aprila Bank.
+        </p>
+      </div>
+    </div>
+    <!-- https://www.npmjs.com/package/aprilabank -->
+    <!-- https://aprilabank-storybook.netlify.app/?path=/docs/getting-started-development--page -->
+
     <h4>Development</h4>
-    <b>Technologies: </b> VueJS, little bit of Blazor
-    <p>I was updating their VueJS application about new features.</p>
+    <b>Technologies: </b> VueJS, Blazor Developer
+    <p>
+      As the only team member with experience in Vue.js, I was responsible for
+      rewriting the architecture of the customer registration form to meet
+      updated requirements. The application required a bit of reverse
+      engineering, as it had originally been built by a highly skilled senior
+      front-end developer. Refactoring such well-written code was both
+      intellectually rewarding and a pleasure—it gave me the opportunity to
+      explore advanced and clever uses of Vue.js.
+    </p>
+    <p>
+      Due to fact that UX Lead was working in Sketch I needed to begin using
+      Mac. To work with the .NET framework on macOS, I set up a virtual Windows
+      desktop using VirtualBox. This allowed me to contribute to Blazor
+      development despite the limited support for .NET on Mac at the time.
+    </p>
+    <h4>Outcome</h4>
+    <p>
+      Aprila Bank’s needs for feature updates, design system maintenance, and
+      development had been met. I genuinely hope that the UX Lead—being the sole
+      UX and UI designer in the company—felt supported and understood,
+      especially in his efforts to help the development team grasp his vision,
+      understand its importance, and feel encouraged to follow his expertise.
+    </p>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
