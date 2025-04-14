4c062d91dda428cd2a8f9d78ce07cc70c4aca60f
aniWeyn <4niweyn@gmail.com>
2025-04-14 10:43:45 +0200

    updated crayon

--- src/views/Crayon.vue
+++ src/views/Crayon.vue
@@ -2,15 +2,33 @@
   <div class="about">
     <h2>Crayon</h2>
     <p class="project-description">
-      This project was ordered to test the survey concept. We can call it "rapid
-      prototyping". Survey questions and replies are saved in json file. The
-      result of survey were saved in Firabse.
+      Crayon is a global IT services and consulting company headquartered in
+      Oslo, Norway, with over 4,000 employees across 46 countries. Specializing
+      in cloud solutions, software asset management, artificial intelligence,
+      and digital transformation, Crayon empowers organizations to optimize
+      their technology investments and drive innovation.
     </p>
     <b>Roles:</b> UX / UI Designer, VueJS Developer
-    <div class="about"></div>
     <p><b>Technology: </b> VueJS, Firebase</p>
+    <p>
+      Crayon wanted to test a new customer screening survey with a complex
+      structure that standard tools like SurveyMonkey couldn’t support. I
+      contributed by reviewing the survey from a research perspective (e.g.,
+      identifying leading questions) and primarily by prototyping the survey to
+      enable user testing and evaluation of its usability.
+    </p>
+    <h4>VueJS Developer</h4>
+    <p>
+      I have choosen VueJS for this project due to fact that at that time that
+      was the lightest framework for rapid prototyping. Survey questions and
+      replies were saved in json file. The result of survey were saved in
+      Firabse.
+    </p>
+    <h4>Outcome</h4>
+    <p>
+      Sales team could quickly test their ideas and move forwards with project.
+    </p>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
