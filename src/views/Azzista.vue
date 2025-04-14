2bd9ea98ee796c53803d267aa914f8b000a9e28f
aniWeyn <4niweyn@gmail.com>
2025-04-14 15:14:47 +0200

    updated azzista

--- src/views/Azzista.vue
+++ src/views/Azzista.vue
@@ -1,47 +1,150 @@
 <template>
   <div>
     <h2>Azzista Sports Innovation</h2>
+    <p class="project-description">
+      Azzista Sports Innovation (now Dribblr) is a value-driven sports
+      technology company based in Larvik, Norway, dedicated to connecting the
+      global football community through a digital platform that bridges elite
+      and grassroots levels. Their mission focuses on reducing disparities,
+      promoting equality, and providing development opportunities for players,
+      coaches, and clubs worldwide.
+    </p>
     <div class="about">
-      <b>Roles:</b> UX / UI Designer, React Developer
+      <b>Roles:</b> UX / UI Designer, React Developer, Project Management
+      <h3>Phase 1</h3>
+      <h4>UX / UI Designer</h4>
+      <b>Technology:</b>
+      Figma, Storybook, ChakraUI <br />
+      <p>
+        I was invited to join a team of three developers—two junior and one
+        senior—with the task of reviewing the client-provided information and
+        creating a prototype to guide the development process. My role was to
+        clarify requirements and translate them into a clear, usable design that
+        the team could implement effectively. I created a high-fidelity
+        prototype in Figma, designed with Chakra UI as the intended framework.
+        The prototype served both as a visual guide for the development team and
+        as a tool to ensure alignment between the developers and the
+        stakeholders.
+      </p>
+      <div class="image-description">
+        <div>
+          <img
+            src="~@/assets/img/azzista_example1.png"
+            alt="Screenshot of Figma prototype"
+            width="100%"
+            height="auto"
+          />
+          <p class="paragraph">
+            The screenshot above showcases first interpretations of
+            stakeholders' vision.
+          </p>
+        </div>
+      </div>
+      <div class="image-description">
+        <div>
+          <img
+            src="~@/assets/img/azzista_example3.png"
+            alt="Screenshot of Figma prototype"
+            width="100%"
+            height="auto"
+          />
+          <p class="paragraph">
+            The screenshot above showcases first interpretations of
+            stakeholders' vision.
+          </p>
+        </div>
+      </div>
       <h3>Phase 2</h3>
+      <h4>UX / UI Designer</h4>
+      <b>Technology:</b>
+      AdobeXD, co-design workshops, usability testing, competition comparison
+      <br />
       <p>
-        <b>UX:</b>
-        Workshops <br />
-        working with the customer to help express the vision of the application
-        <br />
-        preparing high-fidelity prototypes
+        In the second phase, I had the opportunity to work directly with the
+        stakeholder through a series of <strong>co-design</strong> sessions. I
+        saw this as a valuable moment to incorporate principles from
+        <strong>socially ethical design</strong>, as proposed by
+        <a href="https://edelwax.medium.com/" target="_blank">Joe Edelman</a>,
+        along with ideas from
+        <a href="https://www.humanetech.com/" target="_blank">Tristan Harris</a>
+        and the Center for Humane Technology— ensuring the application supports
+        screen-free time while still enabling young football players to share
+        their achievements and feel inspired by the progress of others.
       </p>
       <p>
-        <b>UI / UX:</b>
-        AdobeXD <br />
-        visual identity created by graphic designer (Dimitri), <br />
-        then I edited pages and created new pages based on created visual
-        identity
+        With a strong understanding of the project and its mission, I had the
+        opportunity to invite a senior graphic designer to help us establish the
+        visual identity for the project. We transitioned the prototype to Adobe
+        XD, where I continued the work as a UX designer using UI components
+        developed by the graphic designer.
       </p>
       <p>
-        <b>Technology:</b>
-        React, ChakraUI overwritten by SASS styling <br />
-        use of CSS Variables and css styling to let developers create components
-        in the most comfortable way for them with assuring consisten look
-        through all application, I was responsible as well for layouting and
-        components development
+        In this phase, I was responsible for designing both the web and mobile
+        versions of the application. While the senior graphic designer focused
+        solely on the mobile experience, it was my responsibility to ensure that
+        the web version accurately reflected the visual style and functionality
+        of the mobile design.
+      </p>
+      <p>
+        For the project, I also conducted a
+        <strong>competitive analysis</strong> to ensure that the application
+        offers added value not found in other similar apps. We discovered that
+        the app could differentiate itself by including social-media-like
+        content, allowing football players to express themselves through their
+        profiles.
+      </p>
+      <p>
+        As testing with potential end-users was not permitted, I ensured the
+        clarity and usability of the application's features by conducting
+        usability tests with the development team and available consultants who
+        were not directly involved in the project.
+      </p>
+      <h4>Project Management</h4>
+      <b>Technology:</b> Azure DevOps, Agile, Kanban
+      <p>
+        I contributed to project management by breaking down and describing
+        features and UI elements, creating tasks in the Azure DevOps board, and
+        making decisions on task prioritization. I also followed up on the
+        team’s progress to ensure alignment with the project goals and
+        timelines. My responsibilities were focused on the UX and UI aspects of
+        the application, while infrastructure-related tasks were defined and
+        managed by other developers. The team worked within an Agile framework
+        supported by regular Scrum meetings, which helped maintain clear
+        communication and steady progress.
+      </p>
+      <h4>React Developer</h4>
+      <b>Technology:</b>
+      React, ChakraUI with elements of SASS and CSS Variables <br />
+      <p>
+        Our team of five growth to seven developers. use of CSS Variables and
+        css styling to let developers create components in the most comfortable
+        way for them with assuring consisten look through all application, I was
+        responsible as well for layouting and components development.
+      </p>
+      <p>
+        For example, I had the opportunity to create a football field layout
+        using CSS for the web solution:
+        <a href="https://codepen.io/AniWeyn/full/oNLRKWE" target="_blank"
+          >CodePen – Football Field</a
+        >. This component was later adapted within the project to allow users to
+        select their position on the field.
       </p>
       <p>
         <b>Quality Assurance:</b><br />
-        Reviewing Pull Requests both as a designer and as a developer, <br />
-        helping junior developers
+        I reviewed pull requests both as a designer and a developer, ensuring
+        consistency in UI implementation and design intent. I also supported
+        junior developers by providing guidance on semantic HTML, CSS Grid,
+        Flexbox, ChakraUI usage.
       </p>
-      <h3>Phase 1</h3>
+      <h4>Outcome</h4>
       <p>
-        <b>UI / UX:</b>
-        Figma, Storybook <br />
-        working with developers to achieve common understanding of features,
-        <br />
-        sketches made by me with purpose of making the development easy
-      </p>
-      <p>
-        <b>Technology:</b>
-        React, ChakraUI
+        The stakeholder received the first functional version of the product,
+        which they were able to use for securing funding, attracting investors,
+        and inviting early access users to gather feedback and insights about
+        the concept. The project now exists under the name
+        <a href="https://www.dribblr.io/" target="_blank">Dribbl</a>, where many
+        of the original ideas were retained, along with a continued strong
+        emphasis on core values.
       </p>
     </div>
   </div>
