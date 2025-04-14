1063de4e6e647254eca15ab7555193ecec708644
aniWeyn <4niweyn@gmail.com>
2025-04-14 15:23:44 +0200

    updated w3schools

--- src/views/W3schools.vue
+++ src/views/W3schools.vue
@@ -2,48 +2,54 @@
   <div class="about">
     <h2>W3schools</h2>
     <p class="project-description">
-      W3schools is free platform to learn HTML, CSS, JavaScript and other technologies. 
+      W3Schools is a Norwegian-based educational website founded in 1998 by Jan
+      Egil Refsnes, offering free and interactive tutorials on web development
+      technologies such as HTML, CSS, JavaScript, Python, and SQL. Known for its
+      beginner-friendly approach, it features the "Try It Yourself" editor,
+      enabling users to experiment with code directly in the browser.
     </p>
-    <h4>UX / UI Design</h4>
-    I have designed UI based on provided requrements and collaboration with W3school.
+    <section><b>Roles:</b> UX / UI Designer</section>
+    <section><b>Project timeline:</b> a few days</section>
+    <h4>UX / UI Designer</h4>
+    I have designed UI based on provided requrements and collaboration with
+    W3school.
 
-       
-       <div>
-           <h5>Final design</h5>
-                          <img
-          src="~@/assets/img/w3schools04.png"
-          alt="Photo of the scale from the front"
-          width="800"
-          height="auto"
-        />
-                <h5>Third iteration</h5>
-        <img
-          src="~@/assets/img/w3schools03.png"
-          alt="Photo of the scale from the front"
-          width="800"
-          height="auto"
-        />
-          <h5>Second iteration</h5>
-        <img
-          src="~@/assets/img/w3schools02.png"
-          alt="Photo of the scale from the front"
-          width="800"
-          height="auto"
-        />
+    <div>
+      <h5>Final design</h5>
+      <img
+        src="~@/assets/img/w3schools04.png"
+        alt="Photo of the scale from the front"
+        width="800"
+        height="auto"
+      />
+      <h5>Third iteration</h5>
+      <img
+        src="~@/assets/img/w3schools03.png"
+        alt="Photo of the scale from the front"
+        width="800"
+        height="auto"
+      />
+      <h5>Second iteration</h5>
+      <img
+        src="~@/assets/img/w3schools02.png"
+        alt="Photo of the scale from the front"
+        width="800"
+        height="auto"
+      />
 
-        <h5>First iteration</h5>
-        <img
-          src="~@/assets/img/w3schools01.png"
-          alt="Photo of the scale from the front"
-          width="800"
-          height="auto"
-        />
-        </div>
+      <h5>First iteration</h5>
+      <img
+        src="~@/assets/img/w3schools01.png"
+        alt="Photo of the scale from the front"
+        width="800"
+        height="auto"
+      />
+    </div>
   </div>
 </template>
 
 <style scoped>
-img{
-  padding: 20px
+img {
+  padding: 20px;
 }
 </style>
